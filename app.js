899099aec31cefa0548fc79be84e47e44d134a44
denyslevada <levada.denys@gmail.com>
2022-09-13 15:01:51 +0300

    level 1: user/paswwarod auth COMPLETED

--- app.js
+++ app.js
@@ -1,1 +1,66 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/userDB')
+
+const userSchema = {
+    email: String,
+    password: String
+}
+
+const User = new mongoose.model('User', userSchema)
+// username=aasd%40sd.sd&password=testPassword
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.route('/login')
+    .get((req, res) => {
+        res.render('login')
+    })
+    .post(async (req, res) => {
+        const {username, password} = req.body
+        User.findOne({email: username}, (err, foundUser) => {
+            if (err) {
+                console.log('err')
+            } else {
+                if (foundUser) {
+                    if (foundUser.password === password) {
+                        res.render('secrets')
+                        return
+                    }
+                }
+            }
+            res.sendStatus(401)
+        })
+
+
+    })
+
+app.route('/register')
+    .get((req, res) => {
+        res.render('register')
+    })
+    .post(async (req, res) => {
+        try {
+            const newUser = new User({
+                email: req.body.username,
+                password: req.body.password
+            })
+
+            await newUser.save()
+            res.render('secrets')
+        } catch (e) {
+            res.send(e)
+        }
+    })
+
+app.listen(port, () => console.log(`Server is running on port: ${port}`))
