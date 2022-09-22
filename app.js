ab27a09f0a6ec0e6879bcddcd608604c72c68137
denyslevada <levada.denys@gmail.com>
2022-09-22 11:05:37 +0300

    level5: passport.js

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
+const Schema = mongoose.Schema
 
-const saltRounds = 10
+const session = require('express-session')
+const passport = require('passport')
+const passportLocalMongoose = require('passport-local-mongoose')
 
 require('dotenv').config()
 
-mongoose.connect('mongodb://localhost:27017/userDB')
-
-const Schema = mongoose.Schema
-
-const userSchema = new Schema({
-    email: String,
-    password: String
-})
-
-const User = new mongoose.model('User', userSchema)
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -23,33 +15,69 @@ const port = process.env.PORT || 3000
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
+app.use(session({
+    secret: 'Our little secret.',
+    resave: false,
+    saveUninitialized: false,
+
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+mongoose.connect('mongodb://localhost:27017/userDB')
+
+
+const userSchema = new Schema({
+    email: String,
+    password: String
+})
+
+userSchema.plugin(passportLocalMongoose)
+
+const User = new mongoose.model('User', userSchema)
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+app.get('/secrets', (req, res) => {
+    if (req.isAuthenticated()) {
+        res.render('secrets')
+    } else {
+        res.redirect('/login')
+    }
+})
+
+app.get('/logout', (req,res) => {
+    req.logout(() => res.redirect('/'))
+
 })
 
 app.route('/login')
     .get((req, res) => {
         res.render('login')
     })
-    .post(async (req, res) => {
-        const {username, password} = req.body
+    .post((req, res) => {
+        const user = new User({
+            username: req.body.username,
+            password: req.body.password
+        })
 
-        User.findOne({email: username})
-            .then((foundUser) => {
-                if (!foundUser) {
-                    res.sendStatus(401)
-                } else {
-                    bcrypt.compare(password, foundUser.password)
-                        .then(isPasswordsEqual => {
-                            if (isPasswordsEqual == true) {
-                                res.render('secrets')
-                                return
-                            }
-                            res.sendStatus(401)
-                        })
-                }
-            }).catch(e => res.sendStatus(401))
+        req.login(user, err => {
+            if (err) {
+                console.log(err)
+            } else {
+                passport.authenticate('local')(req, res, () => {
+                    res.redirect('/secrets')
+                })
+            }
+        })
     })
 
 
@@ -57,23 +85,20 @@ app.route('/register')
     .get((req, res) => {
         res.render('register')
     })
-    .post(async (req, res) => {
+    .post((req, res) => {
+        User.register({username: req.body.username}, req.body.password, (err, user) => {
+                if (err) {
+                    console.log(err)
+                    res.redirect('/register')
+                } else {
+                    passport.authenticate('local')(req, res, () => {
+                        console.log(user)
+                        res.redirect('/secrets')
 
-        bcrypt.hash(req.body.password, saltRounds)
-            .then(async (hash) => {
-                try {
-                    const newUser = new User({
-                        email: req.body.username, password: hash
                     })
-
-                    await newUser.save()
-                    res.render('secrets')
-
-                } catch (e) {
-                    res.send(e)
                 }
-            })
-            .catch(e => res.send(e))
+            }
+        )
     })
 
 app.listen(port, () => console.log(`Server is running on port: ${port}`))
